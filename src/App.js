0dbbd63444bfd44cfe172aaa7f434ee1a2a77b58
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-12-15 12:30:13 +0100

    The newHeadlines command implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+const alanKey = '5c4f962aef9977f3f5367d66e9e73fee2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+    const [newsArticles, setNewsArticles] = useState([]);
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({command, articles}) => {
+                if(command === 'newHeadlines') {
+                    setNewsArticles(articles);
+                }
+            }
+        })
+    }, []);
     return (
         <div>
             <h1>Alan AI News Application</h1>
